fec4a2efa5e81f975749d43deccfbbcf779bed3f
Adem Aber Aouni <ademaberaouni@gmail.com>
2020-04-20 15:18:52 -0400

    Adding NodeIcon

--- src/components/Sentinel/SentinelGraph.vue
+++ src/components/Sentinel/SentinelGraph.vue
@@ -1,73 +1,116 @@
 <template>
   <div id="sentinel_graph">
+    <NodeIcon :style="iconStyle" v-bind:hash="sentinel.address" />
   </div>
 </template>
 
 <script>
 import NetworkLink from "../../store/Models/NetworkLink";
-import * as d3 from 'd3';
+import NodeIcon from "../NodeIcon";
+import * as d3 from "d3";
 
 export default {
-    name: 'SentinelGraph',
-    props: ['sentinel'],
-    data() {
-        return{
-            options: {
-                nodeLabels: true,
-                strLinks: true,
-                nodeSize: 8,
-                force: 1500
-            }
-        }
+  name: "SentinelGraph",
+  props: ["sentinel"],
+  components: {
+    NodeIcon
   },
-  computed:{
-    devices(){
-        return this.sentinel.devices;
+  data() {
+    return {
+      options: {
+        nodeLabels: true,
+        strLinks: true,
+        nodeSize: 8,
+        force: 1500
+      },
+      iconStyle: "translate(10px,10px);"
+    };
+  },
+  computed: {
+    devices() {
+      return this.sentinel.devices;
     },
-    nodes(){
-        let nodes = []
-        nodes.push({id: this.sentinel.address, name: this.shortAddress(this.sentinel.address), type: 0})
+    nodes() {
+      let nodes = [];
+      nodes.push({
+        id: this.sentinel.address,
+        name: this.shortAddress(this.sentinel.address),
+        type: 0
+      });
 
-        this.devices.forEach(device => nodes.push({id: device.$id, name: device.name, type: 1}))
+      this.devices.forEach(device =>
+        nodes.push({ id: device.$id, name: device.name, type: 1 })
+      );
 
-        let firstNeighborsLinks = NetworkLink.query().where('source', this.sentinel.address).orWhere('target', this.sentinel.address).get();
-        firstNeighborsLinks.forEach(netLink => {
-            let exists = nodes.some(node => node.id === netLink.source);
-            if (!exists) nodes.push({id: netLink.source, name: this.shortAddress(netLink.source), type: 2});
-            exists = nodes.some(node => node.id === netLink.target);
-            if (!exists) nodes.push({id: netLink.target, name: this.shortAddress(netLink.target), type: 2});
-        });
+      let firstNeighborsLinks = NetworkLink.query()
+        .where("source", this.sentinel.address)
+        .orWhere("target", this.sentinel.address)
+        .get();
+      firstNeighborsLinks.forEach(netLink => {
+        let exists = nodes.some(node => node.id === netLink.source);
+        if (!exists)
+          nodes.push({
+            id: netLink.source,
+            name: this.shortAddress(netLink.source),
+            type: 2
+          });
+        exists = nodes.some(node => node.id === netLink.target);
+        if (!exists)
+          nodes.push({
+            id: netLink.target,
+            name: this.shortAddress(netLink.target),
+            type: 2
+          });
+      });
 
-        nodes.forEach(node => {
-            let address = node.id
-            let secNeighbors = NetworkLink.query().where('source', address).orWhere('target', address).get();
-            secNeighbors.forEach(netLink => {
-                let exists = nodes.some(node => node.id === netLink.source);
-                if (!exists) nodes.push({id: netLink.source, name: this.shortAddress(netLink.source), type: 3});
-                exists = nodes.some(node => node.id === netLink.target);
-                if (!exists) nodes.push({id: netLink.target, name: this.shortAddress(netLink.target), type: 3});
+      nodes.forEach(node => {
+        let address = node.id;
+        let secNeighbors = NetworkLink.query()
+          .where("source", address)
+          .orWhere("target", address)
+          .get();
+        secNeighbors.forEach(netLink => {
+          let exists = nodes.some(node => node.id === netLink.source);
+          if (!exists)
+            nodes.push({
+              id: netLink.source,
+              name: this.shortAddress(netLink.source),
+              type: 3
+            });
+          exists = nodes.some(node => node.id === netLink.target);
+          if (!exists)
+            nodes.push({
+              id: netLink.target,
+              name: this.shortAddress(netLink.target),
+              type: 3
             });
         });
-        console.log(nodes)
-        return nodes
+      });
+      console.log(nodes);
+      return nodes;
     },
     links() {
-        let links = [];
-        this.devices.forEach(device => links.push({target: device.$id, source: this.sentinel.address}))
-        this.nodes.forEach(node => {
-            let address = node.id
-            let netLinks = NetworkLink.query().where('source', address).get();
-            netLinks.forEach(netLink => {
-                let exists = this.nodes.some(node => node.id === netLink.target);
-                if(exists) links.push({target: netLink.target, source: address});
-            })
+      let links = [];
+      this.devices.forEach(device =>
+        links.push({ target: device.$id, source: this.sentinel.address })
+      );
+      this.nodes.forEach(node => {
+        let address = node.id;
+        let netLinks = NetworkLink.query()
+          .where("source", address)
+          .get();
+        netLinks.forEach(netLink => {
+          let exists = this.nodes.some(node => node.id === netLink.target);
+          if (exists) links.push({ target: netLink.target, source: address });
         });
-        console.log(links)
+      });
+      console.log(links);
       return links;
-    }
+    },
+    
   },
   methods: {
-    shortAddress(address){
+    shortAddress(address) {
       return address.slice(0, 3) + address.slice(-3);
     },
     drag(simulation) {
@@ -99,8 +142,12 @@ export default {
     const nodes = this.nodes;
     const links = this.links;
 
-    let width = document.getElementById("sentinel_graph").clientWidth
-    let height = document.getElementById("sentinel_graph").clientHeight
+    let width = document.getElementById("sentinel_graph").clientWidth;
+    let height = document.getElementById("sentinel_graph").clientHeight;
+
+    this.height = height;
+    this.width= width;
+    this.iconStyle = "translate(" + this.width / 2 + "px, " + this.height / 2 + "px);";
 
     const svg = d3
       .selectAll("#sentinel_graph")
@@ -109,7 +156,7 @@ export default {
       .attr("height", height)
       .append("g");
 
-    let radius = 0.1 * Math.min(width,height);
+    let radius = 0.1 * Math.min(width, height);
     svg
       .append("circle")
       .attr("r", radius * 1.5)
@@ -161,7 +208,10 @@ export default {
     // Create a simulation with basic forces and connect nodes with links
     const simulation = d3
       .forceSimulation(nodes)
-      .force("link",d3.forceLink(links).id(d => d.id))
+      .force(
+        "link",
+        d3.forceLink(links).id(d => d.id)
+      )
       .force("charge", d3.forceManyBody())
       .force("center", d3.forceCenter(width / 2, height / 2))
       .force("CenterSelected", () => {
@@ -254,7 +304,7 @@ export default {
       labels.attr("transform", d => "translate(" + d.x + "," + d.y + ")");
     });
   }
-}
+};
 </script>
 <style>
 </style>
